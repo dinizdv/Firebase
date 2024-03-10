9373e9ee2fcf264587b3d49eea46e0137fc491e1
dinizdv <dinizdev3@gmail.com>
2024-03-10 08:45:34 -0300

    feat(firebase): (1) new project created; (2) adding firebase configs to React project.

---
+++ fire-contents/src/App.js
@@ -0,0 +1,13 @@
+import {} from './firebaseConnection'
+
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React + Firebase</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ fire-contents/src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import {initializeApp} from 'firebase/app'
+import {getFirestore} from 'firebase/firestore' // database (firestore - NoSQL)
+
+// my informations
+const firebaseConfig = {
+    apiKey: "AIzaSyBhzfjgqPsB8inDEurxuNjljqi-if1-t9I",
+    authDomain: "test-8c67d.firebaseapp.com",
+    projectId: "test-8c67d",
+    storageBucket: "test-8c67d.appspot.com",
+    messagingSenderId: "655113243432",
+    appId: "1:655113243432:web:bff8d7f8e5c8a7476dde1d",
+    measurementId: "G-4Z67GRSXM2"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig) // initializing of the firebase (initializeApp get´s the object with configs)
+
+const db = getFirestore(firebaseApp)
+
+export {db}
