a767d371befec2b48be11d38a8650b7b39bd40c9
dinizdv <dinizdev3@gmail.com>
2024-03-16 19:04:34 -0300

    feat(App.js): learning to update the database.

--- fire-contents/src/App.js
+++ fire-contents/src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import {db} from './firebaseConnection'
-import {doc, setDoc, collection, addDoc, getDoc, getDocs} from 'firebase/firestore' // to register item
+import {doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc} from 'firebase/firestore' // to register item
 
 import './index.css'
 
@@ -8,6 +8,8 @@ function App() {
 
   const[title, setTitle] = useState('') // textarea value
   const[author, setAuthor] = useState('') // input value
+  const [idPost, setIdPost] = useState ('')
+
   const [posts, setPosts] = useState([])
 
   async function handleAdd(){
@@ -59,6 +61,23 @@ function App() {
     // })
   }
 
+  async function editPost() {
+    const docRef = doc(db, "posts", idPost)
+    await updateDoc(docRef, {
+      title: title,
+      author: author
+    })
+    .then(() => {
+      console.log("Updated post")
+      setIdPost('')
+      setTitle('')
+      setAuthor('')
+    })
+    .catch((error) => {
+      console.log("ERROR! " + error)
+    })
+  }
+
 
   return (
     <div className="App">
@@ -66,20 +85,24 @@ function App() {
       <div className="container">
         
       <h1>React + Firebase</h1>
+        <label>ID do post:</label>
+        <input value={idPost} onChange={(e) => setIdPost(e.target.value)}></input>
         <label>Title</label>
-        <textarea type="text" placeholder='type the title...' value={title} onChange={ (e) => setTitle(e.target.value)}/>
+        <textarea type="text" placeholder='type the title...' value={title} onChange={ (e) => setTitle(e.target.value)}/> <br/>
 
         <label>Author</label>
         <input type="text" placeholder='Author of the post' value={author} onChange={(e) => setAuthor(e.target.value)} />
 
         <button onClick={handleAdd}>Registrer</button>
         <button onClick={searchPost}>Search post</button>
+        <button onClick={editPost}>Update post</button>
 
         <ul>
           {posts.map((post) => {
             return (
               <li key={post.id}>
-                <span>Title: {post.title} </span> <br/>
+                <strong>Id: {post.id}</strong><br/>
+                <span>Title: {post.title}</span> <br/>
                 <span>Author: {post.author}</span> <br/>
               </li>
             )
